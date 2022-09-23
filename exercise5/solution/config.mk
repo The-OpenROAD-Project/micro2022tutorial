cfb4538556c3d5e8db4da70e580e922f9a4284df
Austin Rovinski <rovinski@umich.edu>
2022-09-23 07:54:49 -0400

    exercise5
    Signed-off-by: Austin Rovinski <rovinski@umich.edu>

---
+++ exercise5/solution/config.mk
@@ -0,0 +1,10 @@
+export DESIGN_NAME = counter
+export PLATFORM    = asap7
+
+export VERILOG_FILES = ../../exercise5/${DESIGN_NAME}.v
+export SDC_FILE      = ../../exercise5/solution/constraint.sdc
+
+export CORE_UTILIZATION = 20
+export PLACE_DENSITY = 0.55
+export CORE_ASPECT_RATIO = 1
+export CORE_MARGIN = 1.0
